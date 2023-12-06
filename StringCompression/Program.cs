537203fcbff81a4577f81df33c6346ac701493dd
Aleksandra Budzikowska <107808781+abudzikowsk@users.noreply.github.com>
2023-12-06 16:02:10 +0100

    Added StringCompression project
    This commit introduces a new console application, "StringCompression", into the repository. The application accepts a string and outputs a compacted version of the string, which displays how many times each character repeats consecutively. The necessary project files, program source code, and assembly information files are included. A .gitignore file has also been added to avoid committing unnecessary files.

---
+++ StringCompression/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace StringCompression
+
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the string: ");
+            string inputString = Console.ReadLine().Trim();
+
+            string outputString = CompressString(inputString);
+            Console.WriteLine(outputString);
+        }
+
+        static string CompressString(string s)
+        {
+            StringBuilder compressedSequence = new StringBuilder();
+            int count = 1;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    compressedSequence.Append($"({count}, {s[i - 1]}) ");
+                    count = 1;
+                }
+            }
+
+            compressedSequence.Append($"({count}, {s[s.Length - 1]})");
+
+            return compressedSequence.ToString();
+        }
+    }
+}
